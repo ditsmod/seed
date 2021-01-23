c7062eff6bec8d9ffeae91725f6cbcf6f280dece
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-01-23 12:48:47 +0200

    Simplify LoggerService

--- src/app/services-per-app/logger.service.ts
+++ src/app/services-per-app/logger.service.ts
@@ -1,28 +1,12 @@
 import { Injectable } from '@ts-stack/di';
-import { Logger } from '@ts-stack/ditsmod';
+import { Logger, LoggerMethod } from '@ts-stack/ditsmod';
 
 @Injectable()
 export class LoggerService extends Logger {
-  trace = (...args: any[]): any => {
+  trace: LoggerMethod = (...args: any[]) => {
     if (!args.length) {
       return true;
     }
-    console.log('-------------- Log trace -------------->', ...args);
-  };
-
-  info = (...args: any[]): any => {
-    console.log('-------------- Log info -------------->', ...args);
-  };
-
-  debug = (...args: any[]): any => {
-    console.log('---------------- Log debug ----------------->', ...args);
-  };
-
-  warn = (...args: any[]): any => {
-    console.log('-------------- Log warn -------------->', ...args);
-  };
-
-  fatal = (...args: any[]): any => {
-    console.log('-------------- Log fatal -------------->', ...args);
+    console.log('Log trace ', ...args);
   };
 }

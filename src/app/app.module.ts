16444c012b2c824173a6ee2106963df88d0136ef
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-04-18 19:05:00 +0300

    Moved openapiModuleWithParams to root module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import * as http from 'http';
 import { RootModule } from '@ditsmod/core';
+import { XOasObject } from '@ts-stack/openapi-spec';
+import { ServiceProvider } from '@ditsmod/core';
+import { OAS_OBJECT, OpenapiModule } from '@ditsmod/openapi';
 
 import { HelloWorldModule } from './modules/routed/hello-world/hello-world.module';
 import { DefaultsModule } from './modules/services/defaults/defaults.module';
+
+const oasObject: XOasObject = { openapi: '3.0.0', info: { title: 'Testing @ditsmod/openapi', version: '1.0.0' } };
+const providersPerApp: ServiceProvider[] = [{ provide: OAS_OBJECT, useValue: oasObject }];
+const openapiModuleWithParams = OpenapiModule.withParams(providersPerApp);
 
 /**
  * Any one of these options are optional.
@@ -13,8 +20,8 @@ import { DefaultsModule } from './modules/services/defaults/defaults.module';
   serverOptions: {},
   listenOptions: { port: 8080, host: 'localhost' },
   prefixPerApp: '',
-  imports: [HelloWorldModule],
-  exports: [DefaultsModule],
+  imports: [HelloWorldModule, openapiModuleWithParams],
+  exports: [DefaultsModule, openapiModuleWithParams],
   controllers: [],
   providersPerApp: [],
   providersPerMod: [],

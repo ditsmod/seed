2ed8a9f996f912bb580704dca27e04d45f6abff1
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-04-18 15:04:25 +0300

    Upgrade to core-beta.12

--- src/app/modules/routed/hello-world/hello-world.module.ts
+++ src/app/modules/routed/hello-world/hello-world.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@ditsmod/core';
+import { XOasObject } from '@ts-stack/openapi-spec';
+import { ServiceProvider } from '@ditsmod/core';
+import { OAS_OBJECT, OpenapiModule } from '@ditsmod/openapi';
 
 import { HelloWorldController } from './hello-world.controller';
 
+const oasObject: XOasObject = { openapi: '3.0.0', info: { title: 'Testing @ditsmod/openapi', version: '1.0.0' } };
+const providersPerApp: ServiceProvider[] = [{ provide: OAS_OBJECT, useValue: oasObject }];
+const openapiModuleWithParams = OpenapiModule.withParams(providersPerApp);
+
 @Module({
+  imports: [openapiModuleWithParams],
+  exports: [openapiModuleWithParams],
   controllers: [HelloWorldController],
 })
 export class HelloWorldModule {}

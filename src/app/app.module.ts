f87d35472d6820a07b09f55b69330069daf62cfb
KostyaTretyak <ktretiak.in.ua@gmail.com>
2022-01-12 22:24:49 +0200

    Added log level to AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import * as http from 'http';
-import { RootModule } from '@ditsmod/core';
+import { Logger, LoggerConfig, RootModule } from '@ditsmod/core';
 import { JwtModule } from '@ditsmod/jwt';
 
 import { HelloWorldModule } from './modules/routed/hello-world/hello-world.module';
 import { DefaultsModule } from './modules/services/defaults/defaults.module';
 import { openapiModuleWithParams } from './modules/services/openapi/openapi.module';
+
+const loggerConfig = new LoggerConfig();
+const level: keyof Logger = 'info';
+loggerConfig.level = level;
 
 /**
  * Any one of these options are optional.
@@ -18,7 +22,7 @@ import { openapiModuleWithParams } from './modules/services/openapi/openapi.modu
   prefixPerApp: '',
   imports: [HelloWorldModule, openapiModuleWithParams],
   controllers: [],
-  providersPerApp: [],
+  providersPerApp: [{ provide: LoggerConfig, useValue: loggerConfig }],
   providersPerMod: [],
   providersPerRou: [],
   providersPerReq: [],

0410cabba99d7d5d525c88718fe307730fbeb421
KostyaTretyak <ktretiak.in.ua@gmail.com>
2023-08-11 16:49:49 +0300

    Renamed function getPort to checkCliAndSetPort

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { RouterModule } from '@ditsmod/router';
 import { BodyParserModule } from '@ditsmod/body-parser';
 
 import { HelloWorldModule } from './modules/routed/hello-world/hello-world.module';
-import { getPort } from './utils/get-port';
+import { checkCliAndSetPort } from './utils/check-cli-and-set-port';
 
 
 /**
@@ -13,7 +13,7 @@ import { getPort } from './utils/get-port';
 @rootModule({
   httpModule: http,
   serverOptions: {},
-  listenOptions: { host: 'localhost', port: getPort(3000) },
+  listenOptions: { host: 'localhost', port: checkCliAndSetPort(3000) },
   path: '',
   appends: [HelloWorldModule],
   imports: [RouterModule, BodyParserModule],

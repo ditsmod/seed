7ee35e14b89a65b206139c38c880d260a2608373
KostyaTretyak <ktretiak.in.ua@gmail.com>
2023-08-20 02:55:30 +0300

    Migration to core-2.44

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,10 @@
-import * as http from 'http';
 import { Providers, rootModule } from '@ditsmod/core';
 import { RouterModule } from '@ditsmod/router';
 import { BodyParserModule } from '@ditsmod/body-parser';
 
 import { HelloWorldModule } from './modules/routed/hello-world/hello-world.module';
-import { checkCliAndSetPort } from './utils/check-cli-and-set-port';
 
 @rootModule({
-  httpModule: http,
-  serverOptions: {},
-  listenOptions: { host: 'localhost', port: checkCliAndSetPort(3000) },
-  path: '',
   appends: [HelloWorldModule],
   imports: [RouterModule, BodyParserModule],
   controllers: [],
--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -4,45 +4,30 @@ import { Providers, Server } from '@ditsmod/core';
 
 import { AppModule } from '../src/app/app.module';
 
-
 describe('Integration tests for HelloWorldController', () => {
   let server: Server;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     jest.restoreAllMocks();
 
     server = await new TestApplication(AppModule)
-      .setInitLogLevel('error')
-      .overrideProviders([
-        ...new Providers().useLogConfig({ level: 'fatal' }),
-      ])
+      .overrideProviders([...new Providers().useLogConfig({ level: 'fatal' })])
       .getServer();
   });
 
-  it('controller works', async () => {
-    await request(server)
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-
+  afterAll(() => {
     server.close();
+  });
+
+  it('controller works', async () => {
+    await request(server).get('/').expect(200).expect('Hello World!');
   });
 
   it('should parsed post', async () => {
-    await request(server)
-      .post('/')
-      .send({ one: 1 })
-      .expect(200)
-      .expect({ one: 1 });
-
-    server.close();
+    await request(server).post('/').send({ one: 1 }).expect(200).expect({ one: 1 });
   });
 
   it('should throw an error', async () => {
-    await request(server)
-      .get('/throw-error')
-      .expect(500);
-
-    server.close();
+    await request(server).get('/throw-error').expect(500);
   });
 });

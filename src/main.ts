f8c328550e41095eebb7f54cc4a6d18841cbcb81
KostyaTretyak <ktretiak.in.ua@gmail.com>
2023-08-29 14:10:36 +0300

    Refactored main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { Application } from '@ditsmod/core';
 import { AppModule } from './app/app.module.js';
 import { checkCliAndSetPort } from './app/utils/check-cli-and-set-port.js';
 
-new Application().bootstrap(AppModule).then((app) => {
-  const port = checkCliAndSetPort(3000);
-  app.server.listen(port, 'localhost');
-});
+const app = await new Application().bootstrap(AppModule);
+const port = checkCliAndSetPort(3000);
+app.server.listen(port, 'localhost');

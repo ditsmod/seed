b3eceba96f6a58c0be0f3c98e74972fedb9453b8
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-08-16 02:09:19 +0300

    refactor: apply new ability for core-2.55.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { BodyParserConfig, BodyParserModule } from '@ditsmod/body-parser';
 import { HelloWorldModule } from './modules/routed/hello-world/hello-world.module.js';
 
 @rootModule({
-  providersPerApp: [
-    ...new Providers()
-      .useLogConfig({ level: 'info' })
-      .useValue<BodyParserConfig>(BodyParserConfig, {
-        jsonOptions: { limit: '100kb' },
-        urlencodedOptions: { extended: true },
-      }),
-  ],
+  providersPerApp: new Providers()
+    .useLogConfig({ level: 'info' })
+    .useValue<BodyParserConfig>(BodyParserConfig, {
+      jsonOptions: { limit: '100kb' },
+      urlencodedOptions: { extended: true },
+    }),
   appends: [HelloWorldModule],
   imports: [RoutingModule, BodyParserModule],
   exports: [RoutingModule, BodyParserModule],
